b306b3832cea039cb511ecbad44f546d10049d41
kareeeeeeen <karnelialengkongg@gmail.com>
2026-06-08 00:51:16 +0700

    pls

--- appfix.py
+++ appfix.py
@@ -14,6 +14,7 @@ st.set_page_config(
 )
 
 def load_css():
+    st.markdown('<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap">', unsafe_allow_html=True)
     with open("sty.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
